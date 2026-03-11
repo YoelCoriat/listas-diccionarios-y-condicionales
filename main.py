54d362cd4f1813f35b6d62ebd819c4ed9c196f09
Jose De Gracia <jodegraciab@gmail.com>
2026-03-11 10:58:36 -0500

    Update main.py
    Se agregaron 5 nombres y 5 edades mas
    Correccion para evitar recorrer todo el diccionario con for.
    Se cambio a busqueda directa (in nombres_dict).
    Se corrigio par que no importe si el nombre esta en minuscula o mayuscula

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ nombres = ['Jose',
            'Roberto',
            'Maria',
            'Gambino',
-           'Gerardo']
+           'Gerardo',
+           'Manolo',
+           'Raul',
+           'Cristina',
+           'Jenifer',
+           'Karina']
 
-nombres_dict = dict(zip(nombres, [
-    17,
-    23,
-    25,
-    20,
-    29]))
+edades = [17, 23, 25, 20, 29, 34, 36, 40, 43, 47]
 
-user_nombre = input("Indique un nombre: ")
-for nombre, edad in nombres_dict.items():
-    if user_nombre.lower() == nombre.lower():
-        print(f"{nombre} tiene {edad} años")
-        break
+nombres_dict = {nombre.lower(): edad for nombre, edad in zip(nombres, edades)}
+
+user_nombre = input("Indique un nombre: ").lower()
+
+if user_nombre in nombres_dict:
+    print(f"{user_nombre.capitalize()} tiene {nombres_dict[user_nombre]} años")
 else:
     print(f"{user_nombre} no se encuentra en el diccionario")
